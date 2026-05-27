7528a72846e820115349525a91c779249f478889
Octar123 <ranjanassmali123@gmail.com>
2026-05-27 17:13:34 +0530

    modified the redis container to AppendOnlyFile(AOF); completed the shortenURL logic by adding the used set in redis to store the used URL's for collision prevension; while refilling check the USED_POOL whether the key already used;

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,10 +1,82 @@
 import { redisClient } from "../config/redis";
+import Url from "../model/Url";
 import { checkAndRefillPool } from "../services/refillPool";
+
+const UNUSED_POOL = 'kgs:unused';
+const USED_POOL = 'kgs:used';
 
 export const shortenUrl = async(req, res) => {
     const {long_url, custom_alias, expiry_date} = req.body;
 
     if(!long_url) return res.status(400).json({error: 'long_url is required'});
 
-    
+    try{
+        let shortUrlId;
+
+        if(custom_alias){
+            shortUrlId = custom_alias.trim();
+
+            if(!/^[a-zA-Z0-9_-]{4,15}$/.test(shortUrlId)){
+                return res.status(400).json({error: 'Custom alais must be 4-15 character long and contains only letters, numbers, hyphens and underscore.'})
+            }
+
+            // const existingAlais = await Url.findOne({shortUrlId});
+            const existingAlais = await redisClient.sIsMember(USED_POOL, shortUrlId); 
+            if(existingAlais){
+                return res.status(409).json({error: 'Custom alais is already taken'});
+            }
+
+            await redisClient.sAdd(USED_POOL, shortUrlId);
+        }else{
+            let looping = true;
+            while(looping){
+
+                shortUrlId = await redisClient.sPop(UNUSED_POOL);
+                
+                if(!shortUrlId){
+                    checkAndRefillPool().catch(err => console.error(`Background refilling failed: ${err}`));
+                    return res.status(503).json({error: 'Sorry for Inconvineance, Service is Temporarily Busy'});
+                }
+                
+                const isUsed = await redisClient.sIsMember(USED_POOL, shortUrlId);
+                if(!isUsed){
+                    looping = false;
+                }
+            }
+            checkAndRefillPool().catch(err => console.error(`Background refilling failed: ${err}`));
+
+            await redisClient.sAdd(USED_POOL, shortUrlId);
+        }
+
+        let expiresAt;
+        if(expiry_date){
+            expiresAt = new Date(expiry_date);
+
+            if(isNaN(expiry_date) || expiresAt <= new Date()){
+                return res.status(400).json({error: 'Expiry must be valid future timestamp'});
+            }
+        }else{
+            expiresAt = undefined;
+        }
+
+        const newUrlMapping = Url.create({
+            shortUrlId,
+            originalUrl: long_url,
+            expiresAt
+        });
+
+        return res.status(201).json({
+            message: 'URL shortened successfully',
+
+        })
+    }catch(err){
+        console.error(`[Controller] Error: ${err}`);
+
+
+        if (error.code === 11000) {
+            return res.status(409).json({ error: 'Short URL key collision occurred. Please try again.' });
+        }
+
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
--- src/services/refillPool.js
+++ src/services/refillPool.js
@@ -1,45 +1,54 @@
 import { redisClient } from "../config/redis";
 import { generateShortKey } from "./keyGenerationService";
 
-const UNUSED_POOL = 'kgs:unused';
+const UNUSED_POOL = "kgs:unused";
+const USED_POOL = "kgs:used";
 const batchSize = 5000;
 const threshold = 2000;
-const LOCK_KEY = 'kgs:refill_lock';
+const LOCK_KEY = "kgs:refill_lock";
 
 export const checkAndRefillPool = async () => {
-    try{
-        const currentSize = await redisClient.sCard(UNUSED_POOL);
-        console.log(`[KGS Logger] Current pool size: ${currentSize}`);
+  try {
+    const currentSize = await redisClient.sCard(UNUSED_POOL);
+    console.log(`[KGS Logger] Current pool size: ${currentSize}`);
 
-        if(currentSize < threshold){
+    if (currentSize < threshold) {
+      const acquiredLock = await redisClient.set(LOCK_KEY, "locked", {
+        condition: "NX",
+        expiration: 10,
+      });
 
-            const acquiredLock = await redisClient.set(LOCK_KEY, 'locked', {
-                condition: 'NX',
-                expiration: 10
-            })
+      if (!acquiredLock) {
+        console.log(
+          `[KGS Logger] Another process is already filling the pool.`,
+        );
+        return;
+      }
 
-            if(!acquiredLock){
-                console.log(`[KGS Logger] Another process is already filling the pool.`);
-                return;
-            }
+      console.log(`[KGS Logger] Pool low, Refilling keys`);
 
-            console.log(`[KGS Logger] Pool low, Refilling keys`);
+      let freshKeysSet = new Set();
 
-            let freshKeysSet = new Set();
+      while (freshKeysSet.size < batchSize) {
+        const key = generateShortKey();
 
-            while(freshKeysSet.size < batchSize){
-                freshKeysSet.add(generateShortKey());
-            }
-
-            const freshKeys = Array.from(freshKeysSet);
-
-            await redisClient.sAdd(UNUSED_POOL, freshKeys);
-            console.log(`[KGS Logger] Pool replenished successfully`);
-
-            await redisClient.del(LOCK_KEY);
+        const isAlreadyUsed = await redisClient.sIsMember(USED_POOL, key);
+        if (!isAlreadyUsed) {
+          freshKeysSet.add(generateShortKey());
         }
-    }catch(error){
-        console.error(`[KGS Logger] Failed to refill pool: ${error}`);
-        await redisClient.del(LOCK_KEY).catch(() => {});
+      }
+
+      const freshKeys = Array.from(freshKeysSet);
+
+      await redisClient.sAdd(UNUSED_POOL, freshKeys);
+      console.log(`[KGS Logger] Pool replenished successfully`);
+
+      await redisClient.del(LOCK_KEY);
     }
-}
+  } catch (error) {
+    console.error(`[KGS Logger] Failed to refill pool: ${error}`);
+    await redisClient.del(LOCK_KEY).catch(() => {});
+  }
+};
+
+/**Note to use Bloom filer for performance, may consider using later if the links are lower than 5 million, if the total links grow beyond that point consider using the bloom filter. */
